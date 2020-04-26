9dab5146b1825507593bda3cc9f5963e14db79eb
mnvssvamsi <60683590+mnvssvamsi@users.noreply.github.com>
2020-04-27 00:43:54 +0530

    Add files via upload
    Frontend

---
+++ Courses/App.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import Courses from'./components/courses'
+
+
+const App =() => {
+  return (
+    <Fragment>
+      <Courses />
+    </Fragment>
+  );
+}
+
+export default App;
+
